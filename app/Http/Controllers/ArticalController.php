6ac2163d4ab2155fc48fdcfb824b7b3f0ffd7821
CodingSeo <seojingyo@hotmail.com>
2020-05-18 15:29:31 +0900

    #article router 및 controller 작성
    -- article router 및 contoller 작성

--- app/Http/Controllers/ArticalController.php
+++ app/Http/Controllers/ArticalController.php
@@ -7,4 +7,25 @@ use Illuminate\Http\Request;
 class ArticalController extends Controller
 {
     //
+    public function index(){
+
+    }
+    public function create(){
+
+    }
+    public function store(){
+
+    }
+    public function show(){
+
+    }
+    public function edit(){
+    	
+    }
+    public function update(){
+    	
+    }
+    public function destory(){
+    	
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/article/','ArticalController@index')
+->name('article.index');
+Route::get('/article/create','ArticalController@create')
+->name('article.create');
+Route::get('/article','ArticalController@store')
+->name('article.store');
+Route::get('/article','ArticalController@show')
+->name('article.show');
+Route::get('/article/{id}','ArticalController@edit')
+->name('article.edit');
+Route::get('/article/{id}/edit','ArticalController@update')
+->name('article.update');
+Route::get('/article/{id}','ArticalController@destory')
+->name('article.destroy');
